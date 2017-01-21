2abff4f64ce37c97f0fee62e1651180c062780fa
Nyxxie <nyxxxxie@gmail.com>
2017-01-21 15:29:52 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Spade
+
+Spade is an experimental rewrite of mycroft in python.  This is being done to
+see how faster python will make development and to solve various issues with
+current mycroft design.  This readme will be updated if this project is chosen
+over mycroft, but until then do not use this library.
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='spade',
+      version='0.1',
+      description='File format reverse engineering tool.',
+      url='https://github.com/nyxxxie/spade',
+      author='Nyxxie et al.',
+      author_email='nyxxxxie@gmail.com',
+      license='GNU GPL v3',
+      packages=['spade'],
+      zip_safe=False)
