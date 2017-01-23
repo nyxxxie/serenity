e5b3bf7deede14b6e7c8f03e8fd5439a1018d963
Nyxxie <nyxxxxie@gmail.com>
2017-01-22 23:00:48 -0600

    Created basic ui.

---
+++ spade/qspade.py
@@ -0,0 +1,16 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+def rungui():
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+    w.resize(250, 150)
+    w.move(300, 300)
+    w.setWindowTitle("Simple")
+    w.show()
+
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    rungui()
