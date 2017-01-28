464e3af3d61c220bfd08d89da98cb5998c17f827
Nyxxie <nyxxxxie@gmail.com>
2017-01-28 02:33:11 -0600

    Changed main qt executable's exception handling style.

--- spade/qspade.py
+++ spade/qspade.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 import sys
+import traceback
 from PyQt5.QtWidgets import QApplication
 from ui.mainwindow import SpadeMainWindow
 
 def rungui(args):
-    app = QApplication(args)
-
-    w = SpadeMainWindow()
-    w.show()
-
-    sys.exit(app.exec_())
+    try:
+        app = QApplication(args)
+        w = SpadeMainWindow()
+        w.show()
+        return app.exec_()
+    except Exception as e:
+        print("Exception!")
+        print(e)
+        traceback.print_exc()
+        return 1
 
 if __name__ == "__main__":
-    rungui(sys.argv)
+    sys.exit(rungui(sys.argv))
