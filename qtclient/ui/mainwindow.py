434ad9e826c0221a7409e21bcfee7188a6b7fed8
Nyxxie <nyxxxxie@gmail.com>
2017-02-02 22:39:39 -0600

    Added menubar and statusbar.

--- qtclient/ui/mainwindow.py
+++ qtclient/ui/mainwindow.py
@@ -1,15 +1,26 @@
 from .editorwidget import EditorWidget
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QDesktopWidget, QMainWindow, QDockWidget, QTextEdit
+from PyQt5.QtWidgets import QAction, QDesktopWidget, QMainWindow, QDockWidget, QTextEdit, qApp
 
 # https://github.com/baoboa/pyqt5/blob/master/examples/mainwindows/dockwidgets/dockwidgets.py
 # http://zetcode.com/gui/pyqt5/firstprograms/
 
 class SpadeMainWindow(QMainWindow):
+    def _init_menubar(self):
+        action_exit = QAction("Exit", self)
+        action_exit.setStatusTip("Exit application")
+        action_exit.triggered.connect(qApp.quit)
+
+        menubar = self.menuBar()
+        menu_file = menubar.addMenu("File")
+        menu_file.addAction(action_exit)
+
     def _init_ui(self):
         self.resize(1200, 800)
         self.center()
         self.setWindowTitle("Spade")
+        self._init_menubar()
+        self.statusBar().showMessage("Ready")
 
     def _create_central_widget(self):
         self.central_widget = EditorWidget()
