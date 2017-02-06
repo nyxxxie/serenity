cbf6f7a3a836d1eba5d6b7d65ab2d396e56d79e3
Nyxxie <nyxxxxie@gmail.com>
2017-02-05 22:50:43 -0600

    Modified project to use sqlalchemy vs raw sqlite.

--- spade/core/project.py
+++ spade/core/project.py
@@ -1,65 +1,63 @@
-import sqlite3
+import datetime
+from sqlalchemy import create_engine, Metadata, Table, Column, Binary, Integer, String, ForeignKey
 
 class Project:
     """Represents an open session for spade."""
 
-    def __initialize_db(self, db):
+    def __create_db(self, engine):
         """
         Creates default tables for a newly created spade project
         """
+        metadata = MetaData()
 
-        # Get cursor for db
-        c = db.cursor()
+        # Define project_info table
+        self.table_pinfo = Table("project_info", metadata,
+            Column("key", String, primary_key=True),
+            Column("value", String)
+        )
 
-        # Execute initialization query
-        rc = db.execute("""
-        PRAGMA foreign_keys = ON; -- need this to use foreign keys
+        # Define files table
+        self.table_files = Table("files", metadata,
+            Column("id", Integer, primary_key=True, autoincrement=True),
+            Column("path", String, unique=True),                # Relative or absolute path to this file
+            Column("hash", Binary),                             # sha256 hash of file contents on last change
+            Column("head_change", Binary, server_default=None), # head change file is at
+        )
 
-        CREATE TABLE project_info(
-            key TEXT UNIQUE,
-            val TEXT
-        );
+        # Define changes table
+        self.table_changes = Table("changes", metadata,
+            Column("file_id", None, ForeignKey("files.id"),     # id of file we apply changes to
+            Column("hash", Binary),                             # sha256 of this change
+            Column("parent", Binary),                           # sha256 of previous change
+            Column("file_pos", Integer),                        # position in file where change occured
+            Column("change_type", Integer),                     # change type (TODO: make this enum).  '+' = insert, '-' = erase, '!' = replace
+            Column("change" Binary)                             # bytes that were inserted or erased
+        )
 
-        CREATE TABLE files(
-            id   INT AUTO_INCREMENT,
-            path TEXT,           -- relative or absolute path to this file
-            hash BLOB,           -- sha256 hash of file contents on last change
-            head_change BLOB,    -- head change the file is at.  default NULL
-            UNIQUE (path),
-            PRIMARY KEY (id)
-        );
+        # Define change_comments table
+        self.table_changes = Table("change_comments", metadata,
+            Column("change", None, ForeignKey("changes.hash"),  # hash of the change this comment refers to
+            Column("text", String)                              # comment text
+        )
 
-        CREATE TABLE changes(
-            file_id INTEGER,     -- file_id of the file we apply changes to
-            hash        BLOB,    -- sha256 of this change
-            parent      BLOB,    -- sha256 of parent (previous) change
-            file_pos,   INTEGER,
-            change_type INTEGER, -- '+' = insert, '-' = erase, '!' = replace
-            change      BLOB,    -- bytes that were inserted or erased
-            FOREIGN KEY (file_id) REFERENCES files (id)
-            UNIQUE (hash)
-        );
-
-        CREATE TABLE changes_comments(
-            hash    BLOB,        -- sha256 of the change this comment refers to
-            comment TEXT,
-            FOREIGN KEY (hash) REFERENCES changes (hash)
-        );
-        """)
+        # Add all tables to the database
+        metadata.create_all(engine)
 
         return True
 
-    def __create_db(self, dbfile):
-        db = sqlite3.connect(dbfile)
-        if !self.__initialize_db(db):
-            return None
-
-        return db;
+    def __add_info(self, key, value, nomodify=False):
+        # TODO: handle nomodify var
+        ins = self.table_pinfo.insert()
+        conn = self__db_engine.connect()
+        conn.execute(ins, key=key, value=value) # TODO: might cause problems?
 
     def __init__(self, dbfile):
         self.__dbfile = dbfile
-        self.__db = self.__create_db(dbfile)
-        # must send query PRAGMA foreign_keys = ON;
+        self.__db_engine = create_engine("sqlite:///"+dbfile)
+        self.__init_db(self.__db_engine)
+        self.__add_info("schema_version", "1")
+        self.__add_info("creation_datetime", datetime.datetime.now(), True)
+        self.__add_info("update_datetime", datetime.datetime.now())
 
     def add_file(self, path):
         """
@@ -74,8 +72,8 @@ class Project:
         """
         pass
 
-    def db(self):
-        return self.__db
+    def db_engine(self):
+        return self.__db_engine
 
     def files(self):
         """
