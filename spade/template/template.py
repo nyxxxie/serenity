4f1040faa34afc509ceddb0c3ffe11465462bd80
Nyxxie <nyxxxxie@gmail.com>
2017-02-15 23:40:22 -0600

    Fleshed out design for template processing.

--- spade/template/template.py
+++ spade/template/template.py
@@ -1,66 +1,52 @@
 import ast
+from ast import Ast, StructDecl, FieldDecl, ArrayDecl
 
 ENTRY_STRUCT = "FILE"
 
 class TemplateException(Exception): pass
+class ParsingException(Exception): pass
 
 # When the user edits a template with the gui, they can select and existing
 # struct in place of a type, and can create newstructs using a struct editor
 # in the template editor system (maybe just have another editor).
 
-#class Field():
-#    def __init__(self, template, ast_def, parent=None):
-#        pass
-#
-#    def name():
-#        """ Returns the name of this field. """
-#        pass
-#
-#    def offset():
-#        """ Returns the offset into a file this field starts at.  """
-#        pass
-#
-#    def size():
-#        """ Returns the size of this field in bytes.  """
-#        pass
-#
-#    def location():
-#        """ Returns the location in the template tree of this field.
-#            For example: "FILE.header.signature".
-#        """
-#        pass
-#
-class Struct():
-    def __init__(self, template, ast_def, parent=None):
-        pass
+class TNode():
+    def __init__(self, template, name, parent):
+        self.template = template
+        self.name = name
+        self.parent = parent
+        self.size = 0
+        self.offset = 0
 
-    def name():
-        """ Returns the name of this struct. """
-        pass
+        # Figure out location
+        self.location = ""
+        if parent is None:
+            self.location += parent.location
+        self.location += ("." + name)
 
-    def offset():
-        """ Returns the offset into a file this struct starts at.
-        """
-        pass
+class TField(TNode):
+    def __init__(self, template, type, name, parent):
+        super.__init__(template, name, parent)
+        self.type = type
 
-    def size():
-        """ Returns the size of this struct in bytes.
-        """
-        pass
+class TArray(TNode):
+    def __init__(self, template, name, parent):
+        super.__init__(template, name, parent)
 
-    def location():
-        """ Returns the location in the template tree of this struct.
-            For example: "FILE.header.signature".
-        """
-        pass
+class TStruct(TNode):
+    def __init__(self, template, name, parent=None):
+        super.__init__(template, name, parent)
+        self.fields = []
 
 class Template():
     """ Programatic representation of a template.  Uses AST definitions to
         generate a workable template that is ready to operate on a file.
     """
 
-    def __init__(self, f, ast):
+    def __init__(self, ast, file=None):
         self.ast = ast
+        self.entry = None
+        self.entry = self.process(ast, file)
 
     def __str__(self):
         return "Template not implemented :c"
@@ -68,39 +54,53 @@ class Template():
     def __repr__(self):
         return self.__str__()
 
-    def set_file(self, f):
-        pass
-
-
-    def get_field(offset=None, path=None):
+    def field(self, offset=None, path=None, scope=None):
         """ Gets field associated with an offset or path.
         """
         pass
 
-    def get_entry():
-        """ Returns the entry point struct """
-        return self._entry
-
-    def process(self):
+    def process(self, ast, file):
         """ Transforms a parsed AST into a workable template. """
-        # Find entry point struct
-        entry_def = None
+
+        # Find entry point struct definition
+        ast_entry = None
         for struct in ast.structs:
             if struct.name == ENTRY_STRUCT:
-                entry_def = struct
+                ast_entry = struct
                 break
+        if ast_entry is None:
+            raise ParsingException("Couldn't find entrypoint.")
 
-        if entry_def is None:
-            return False
+        # Process entry point struct
+        return self._process_struct(ast, file, ast_entry)
 
-        # Create struct
-        self.entry = self._create_struct(entry_def)
-        if self.entry is None:
-            return False
+    def reload(self):
+        """ Updates template after file it is applied to is modified. """
+        self.entry = process(self.ast, self.file)
 
-        return True
+    def _process_struct(self, ast, file, struct_def, parent=None):
+        """ Turns an AST struct definition into a struct template node """
+        struct = TStruct(self, struct_def.name, parent)
 
-    def add_struct_decl(self, struct_decl):
-        """ Creates a template struct from a struct definition. """
-        print("About to construct struct \"%s\"." % struct_def.name)
-        return None
+        # Process each struct field
+        for decl in struct_def.field_list:
+            field = None
+            if decl is FieldDecl:
+                field = self._process_field(ast, file, decl, struct)
+            elif decl is ArrayDecl:
+                field = self._process_array(ast, file, decl, struct)
+            else:
+                raise ParsingException("Encountered unhandled field type \"%s\" while parsing struct \"%s\"."
+                    % (type(decl), struct.location))
+
+        return struct
+
+    def _process_field(self, ast, file, field_def, parent):
+        """ Turns an AST field definition into a field template node """
+        #field = TField(self, decl.type, decl.name, parent)
+        pass
+
+    def _process_array(self, ast, file, array_def, parent:
+        """ Turns an AST array definition into an array template node """
+        #field = TArray(self, decl.type, decl.name, parent)
+        pass
