bcac584003fb28020e0387477548b5457461f32b
Nyxxie <nyxxxxie@gmail.com>
2017-03-09 16:13:09 -0600

    Modified project test fixture to clean up better.

--- tests/test_project.py
+++ tests/test_project.py
@@ -3,10 +3,13 @@ import pytest
 from spade.core.project import Project
 from .utils.project_sql import check_table
 
-dbfile = "tests/testdb.sdb"
+dbfile = "testdb.sdb"
 
 @pytest.fixture()
 def cleanup():
+    print("PRE-TEST")
+    yield
+    print("POST-TEST")
     if os.path.isfile(dbfile):
         os.remove(dbfile)
 
@@ -17,5 +20,3 @@ def test_project_initialization(cleanup):
     # Verify default tables exist
     assert check_table(dbfile, "project_info")
     assert check_table(dbfile, "files")
-    assert check_table(dbfile, "changes")
-    assert check_table(dbfile, "change_comments")
