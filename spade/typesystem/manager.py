18c72df89b54b03756ba21df2332af29a166d6b3
Nyxxie <nyxxxxie@gmail.com>
2017-03-17 11:54:16 -0500

    Implemented the typemanager.

--- spade/typesystem/manager.py
+++ spade/typesystem/manager.py
@@ -1,21 +1,34 @@
+from spade.typesystem.types import default_types
+
 class TypeManager:
     def __init__(self):
-        self.types = {}
+        self._types = {}
+        self._add_default_types()
 
     def _add_default_types(self):
-        for type in default_types:
-            self.add_type(type)
+        for typedef in default_types:
+            self.add_type(typedef)
 
     def add_type(self, typedef):
-        """ Adds a type definition identified by a given name. """
-        types[name] = typedef
+        """
+        Adds a type definition identified by a given name.
+        """
+        self._types[typedef.name] = typedef
 
     def remove_type(self, name):
-        """ Removes a type definition given its name. """
-        return types.pop(name, None)
+        """
+        Removes a type definition given its name.
+        """
+        return self._types.pop(name, None)
 
     def get_type(self, name):
-        """ Gets type definition by its name. """
-        return types.get(name)
+        """
+        Gets type definition by its name.
+        """
+        return self._types.get(name)
 
-default_types = []
+    def types(self):
+        """
+        Returns the string identifiers of all types currently registered.
+        """
+        return list(self._types.keys())
