897f73ed120b243b8d580502a525d3ca427a12f5
Nyxxie <nyxxxxie@gmail.com>
2017-05-25 01:37:32 -0500

    Added basic uint32 implementation.

--- spade/typesystem/types/uint32.py
+++ spade/typesystem/types/uint32.py
@@ -1,15 +1,33 @@
-#from spade.typesystem.typedef import TypeDef
-#from spade.typesystem.types import default_types
-#
-#class UInt32(TypeDef):
-#    def __init__(self):
-#        super().__init__(["uint32"])
-#        self.size = 4
-#
-#    def to_string(self, byte_array: bytes) -> str:
-#        return ""
-#
-#    def from_string(self, string: str) -> bytes:
-#        return ""
-#
-#default_types.append(UInt32())
+from spade.typesystem import typemanager
+from spade.typesystem.typedef import TypeDef, InvalidTypeException, NullDataException
+
+class UInt32(TypeDef):
+    __typenames__ = ["uint32", "uint", "ui32", "ui32le"]
+
+    def to_string(self, data) -> str:
+        if data is None or len(data) == 0:
+            return None
+
+        self._size = 4
+
+        if isinstance(data, bytes):
+            return None
+        elif isinstance(data, str):
+            return None
+        else:
+            raise InvalidTypeException("Data type {} can't be converted.".format(str(type(data))))
+
+    def to_bytes(self, data) -> bytes:
+        if data is None or len(data) == 0:
+            return None
+
+        self._size = 4
+
+        if isinstance(data, bytes):
+            return None
+        elif isinstance(data, str):
+            return None
+        else:
+            raise InvalidTypeException("Data type {} can't be converted.".format(str(type(data))))
+
+typemanager.add_type(UInt32)
