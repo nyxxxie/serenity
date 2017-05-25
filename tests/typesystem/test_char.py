e97ed369c7bfa4bdd64eba4d894b1f6be3c8c284
Nyxxie <nyxxxxie@gmail.com>
2017-05-25 01:36:05 -0500

    Added char test.

--- tests/typesystem/test_char.py
+++ tests/typesystem/test_char.py
@@ -1,36 +1,94 @@
-#from .fixtures import char
-#
-#def test_init(char):
-#    assert "char" in char.names
-#    assert char.size == 1
-#
-#def test_to_string_none(char):
-#    s = char.to_string(None)
-#    assert s is None
-#
-#def test_from_string_none(char):
-#    s = char.from_string(None)
-#    assert s is None
-#
-#def test_from_string_empty(char):
-#    s = char.from_string("")
-#    assert s is None
-#
-#def test_from_string_A(char):
-#    s = char.from_string("A")
-#    assert s is not None
-#    assert len(s) == 1
-#    assert s[0] == 0x41 # hex for 'A' in utf-8 & ascii
-#
-#def test_from_string_unprintable(char):
-#    s = char.from_string("\n")
-#    assert s is not None
-#    assert len(s) == 1
-#    assert s[0] == 0x2e # hex for '.' in utf-8 & ascii
-#
-#def test_to_string_empty(char):
-#    s = char.from_string(bytes([]))
-#    assert s is None
-#
-#def test_to_string_A(char):
-#    pass
+from spade.typesystem import typemanager
+from spade.typesystem.types.byte import Byte
+
+# ---------------------------
+# INIT AND ODD CASES
+# ---------------------------
+def test_init():
+    byte = Byte()
+    assert byte.size() == 0
+    assert byte.string() is None
+    assert byte.bytes() is None
+    assert byte.unprintable()
+
+def test_none():
+    byte = Byte(None)
+    assert byte.size() == 0
+    assert byte.string() is None
+    assert byte.bytes() is None
+    assert byte.unprintable()
+
+def test_empty_bytes():
+    byte = Byte(bytes([]))
+    assert byte.size() == 0
+    assert byte.string() is None
+    assert byte.bytes() is None
+    assert byte.unprintable()
+
+def test_empty_string():
+    byte = Byte("")
+    assert byte.size() == 0
+    assert byte.string() is None
+    assert byte.bytes() is None
+    assert byte.unprintable()
+
+
+# ---------------------------
+# CONVERT A
+# ---------------------------
+def test_convert_letter_string_lower():
+    byte = Byte("A")
+    assert byte.size() == 1
+    assert byte.string() == "A"
+    assert byte.bytes() == bytes([0x41])
+    assert not byte.unprintable()
+
+def test_convert_letter_string_lower():
+    byte = Byte("a")
+    assert byte.size() == 1
+    assert byte.string() == "A"
+    assert byte.bytes() == bytes([0x41])
+    assert not byte.unprintable()
+
+def test_convert_letter_bytes():
+    byte = Byte(bytes([0x41]))
+    assert byte.size() == 1
+    assert byte.string() == "A"
+    assert byte.bytes() == bytes([0x41])
+    assert not byte.unprintable()
+
+
+# ---------------------------
+# CONVERT 7
+# ---------------------------
+def test_convert_number_string():
+    byte = Byte("7")
+    assert byte.size() == 1
+    assert byte.string() == "7"
+    assert byte.bytes() == bytes([0x37])
+    assert not byte.unprintable()
+
+def test_convert_number_bytes():
+    byte = Byte(bytes([0x37]))
+    assert byte.size() == 1
+    assert byte.string() == "7"
+    assert byte.bytes() == bytes([0x37])
+    assert not byte.unprintable()
+
+
+# ---------------------------
+# CONVERT UNPRINTABLE
+# ---------------------------
+def test_convert_number_string():
+    byte = Byte("\n")
+    assert byte.size() == 1
+    assert byte.string() == "\n"
+    assert byte.bytes() == bytes([0x01])
+    assert byte.unprintable()
+
+def test_convert_number_bytes():
+    byte = Byte(bytes([0x01]))
+    assert byte.size() == 1
+    assert byte.string() == "\n"
+    assert byte.bytes() == bytes([0x01])
+    assert byte.unprintable()
