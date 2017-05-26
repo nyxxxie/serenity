ed5e3def6b2902fb1c1322a378bfabcf6c84b3b5
Nyxxie <nyxxxxie@gmail.com>
2017-05-26 01:54:06 -0500

    Fixed bugs in char unit tests.

--- tests/typesystem/test_char.py
+++ tests/typesystem/test_char.py
@@ -1,94 +1,94 @@
 from spade.typesystem import typemanager
-from spade.typesystem.types.byte import Byte
+from spade.typesystem.types.char import Char
 
 # ---------------------------
 # INIT AND ODD CASES
 # ---------------------------
 def test_init():
-    byte = Byte()
-    assert byte.size() == 0
-    assert byte.string() is None
-    assert byte.bytes() is None
-    assert byte.unprintable()
+    char = Char()
+    assert char.size() == 0
+    assert char.string() is None
+    assert char.bytes() is None
+    assert char.unprintable()
 
 def test_none():
-    byte = Byte(None)
-    assert byte.size() == 0
-    assert byte.string() is None
-    assert byte.bytes() is None
-    assert byte.unprintable()
+    char = Char(None)
+    assert char.size() == 0
+    assert char.string() is None
+    assert char.bytes() is None
+    assert char.unprintable()
 
-def test_empty_bytes():
-    byte = Byte(bytes([]))
-    assert byte.size() == 0
-    assert byte.string() is None
-    assert byte.bytes() is None
-    assert byte.unprintable()
+def test_empty_chars():
+    char = Char(bytes([]))
+    assert char.size() == 0
+    assert char.string() is None
+    assert char.bytes() is None
+    assert char.unprintable()
 
 def test_empty_string():
-    byte = Byte("")
-    assert byte.size() == 0
-    assert byte.string() is None
-    assert byte.bytes() is None
-    assert byte.unprintable()
+    char = Char("")
+    assert char.size() == 0
+    assert char.string() is None
+    assert char.bytes() is None
+    assert char.unprintable()
 
 
 # ---------------------------
 # CONVERT A
 # ---------------------------
 def test_convert_letter_string_lower():
-    byte = Byte("A")
-    assert byte.size() == 1
-    assert byte.string() == "A"
-    assert byte.bytes() == bytes([0x41])
-    assert not byte.unprintable()
+    char = Char("A")
+    assert char.size() == 1
+    assert char.string() == "A"
+    assert char.bytes() == bytes([0x41])
+    assert not char.unprintable()
 
 def test_convert_letter_string_lower():
-    byte = Byte("a")
-    assert byte.size() == 1
-    assert byte.string() == "A"
-    assert byte.bytes() == bytes([0x41])
-    assert not byte.unprintable()
+    char = Char("a")
+    assert char.size() == 1
+    assert char.string() == "A"
+    assert char.bytes() == bytes([0x41])
+    assert not char.unprintable()
 
-def test_convert_letter_bytes():
-    byte = Byte(bytes([0x41]))
-    assert byte.size() == 1
-    assert byte.string() == "A"
-    assert byte.bytes() == bytes([0x41])
-    assert not byte.unprintable()
+def test_convert_letter_chars():
+    char = Char(bytes([0x41]))
+    assert char.size() == 1
+    assert char.string() == "A"
+    assert char.bytes() == bytes([0x41])
+    assert not char.unprintable()
 
 
 # ---------------------------
 # CONVERT 7
 # ---------------------------
 def test_convert_number_string():
-    byte = Byte("7")
-    assert byte.size() == 1
-    assert byte.string() == "7"
-    assert byte.bytes() == bytes([0x37])
-    assert not byte.unprintable()
+    char = Char("7")
+    assert char.size() == 1
+    assert char.string() == "7"
+    assert char.bytes() == bytes([0x37])
+    assert not char.unprintable()
 
-def test_convert_number_bytes():
-    byte = Byte(bytes([0x37]))
-    assert byte.size() == 1
-    assert byte.string() == "7"
-    assert byte.bytes() == bytes([0x37])
-    assert not byte.unprintable()
+def test_convert_number_chars():
+    char = Char(bytes([0x37]))
+    assert char.size() == 1
+    assert char.string() == "7"
+    assert char.bytes() == bytes([0x37])
+    assert not char.unprintable()
 
 
 # ---------------------------
 # CONVERT UNPRINTABLE
 # ---------------------------
 def test_convert_number_string():
-    byte = Byte("\n")
-    assert byte.size() == 1
-    assert byte.string() == "\n"
-    assert byte.bytes() == bytes([0x01])
-    assert byte.unprintable()
+    char = Char("\n")
+    assert char.size() == 1
+    assert char.string() == "\n"
+    assert char.bytes() == bytes([0x0A])
+    assert char.unprintable()
 
-def test_convert_number_bytes():
-    byte = Byte(bytes([0x01]))
-    assert byte.size() == 1
-    assert byte.string() == "\n"
-    assert byte.bytes() == bytes([0x01])
-    assert byte.unprintable()
+def test_convert_number_chars():
+    char = Char(bytes([0x0A]))
+    assert char.size() == 1
+    assert char.string() == "\n"
+    assert char.bytes() == bytes([0x0A])
+    assert char.unprintable()
