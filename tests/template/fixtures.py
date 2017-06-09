bc1981edc5841c3a1137d9df5a720834f169dece
Nyxxie <nyxxxxie@gmail.com>
2017-06-09 01:17:29 -0500

    Added another fixture and changed up docstring.

--- tests/template/fixtures.py
+++ tests/template/fixtures.py
@@ -5,8 +5,11 @@ RELATIVE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 @pytest.fixture()
 def singlestruct():
-    """
-    This returns the path to the basictemplate.stf file.
-    """
-    path = os.path.join(RELATIVE_DIR, "testdata", "basictemplate.stf")
+    """This returns the path to the singlestruct.stf file."""
+    path = os.path.join(RELATIVE_DIR, "testdata", "singlestruct.stf")
+    yield path
+
+def singlestruct_target():
+    """This returns the path to the singlestruct_target file."""
+    path = os.path.join(RELATIVE_DIR, "testdata", "singlestruct_target")
     yield path
