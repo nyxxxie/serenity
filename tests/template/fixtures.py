cfcaa5b87a296c257125cf6d747d6bfb2c669328
Nyxxie <nyxxxxie@gmail.com>
2017-07-10 02:30:37 -0500

    Got rid of the preconstructed template test.  It's out of date now and probably useless anyways, so I figure I'll just delete it and add it back later if it's necessary to gain coverage in areas we can't touch with template files alone.

--- tests/template/fixtures.py
+++ tests/template/fixtures.py
@@ -7,47 +7,6 @@ RELATIVE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
 @pytest.fixture()
-def node_tree():
-    """This fixture manually builds the following struct: ::
-
-        struct header_t {
-            int data;
-        };
-
-        struct idk_t {
-            int f;
-        }
-
-        struct thing_t {
-            idk_t idk;
-        };
-
-        struct FILE {
-            int field1;
-            int field2;
-            header_t header;
-            thing_t thing;
-        };
-    """
-
-    root = template.TRoot()
-
-    root.fields.append(template.TField(int32.Int32, "field1", root, root, 0))
-    root.fields.append(template.TField(int32.Int32, "field2", root, root, 4))
-
-    header = template.TStruct("header", root, root, 8)
-    header.fields.append(template.TField(int32.Int32, "data", header, root, 8))
-    root.fields.append(header)
-
-    thing = template.TStruct("thing", root, root, 12)
-    idk = template.TStruct("idk", thing, root, 12)
-    idk.fields.append(template.TField(int32.Int32, "f", idk, root, 12))
-    thing.fields.append(idk)
-    root.fields.append(thing)
-
-    yield root
-
-@pytest.fixture()
 def singlestruct():
     """This returns the path to the singlestruct.stf file."""
     path = os.path.join(RELATIVE_DIR, "testdata", "singlestruct.stf")
@@ -57,4 +16,16 @@ def singlestruct():
 def singlestruct_target():
     """This returns the path to the singlestruct_target file."""
     path = os.path.join(RELATIVE_DIR, "testdata", "singlestruct_target")
+    yield path
+
+@pytest.fixture()
+def multistruct():
+    """This returns the path to the multistruct.stf file."""
+    path = os.path.join(RELATIVE_DIR, "testdata", "multistruct.stf")
+    yield path
+
+@pytest.fixture()
+def multistruct_target():
+    """This returns the path to the multistruct_target file."""
+    path = os.path.join(RELATIVE_DIR, "testdata", "multistruct_target")
     yield path
