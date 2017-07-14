6656b6444eb76333da7b70dc4db8bf8f799da23f
Nyxxie <nyxxxxie@gmail.com>
2017-07-14 02:33:16 -0500

    Stubbed out emptyfile test for now.

---
+++ tests/template/parser/test_emptyfile.py
@@ -0,0 +1,9 @@
+from tests.template.fixtures import emptyfile
+
+from spade.template import parser
+from spade.template import ast
+
+
+#def test_parse_file(emptyfile):
+#    root = parser.TemplateParser.parse_file(emptyfile)
+#    assert root
