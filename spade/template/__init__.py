00b6299fdcc1c81a90c2055efab28697309254f9
Nyxxie <nyxxxxie@gmail.com>
2017-07-23 12:18:01 -0500

    Changed template creation fn to work with the new template parsing model.

--- spade/template/__init__.py
+++ spade/template/__init__.py
@@ -2,7 +2,7 @@ import logging
 from spade.template import parser
 from spade.template import generator
 
-def from_file(target_file: str, template_file: str):
+def from_file(target_file_path: str, template_file: str):
     """Transforms a target + a .stf file into a workable template.
 
     :param stf_file: STF file.
@@ -10,11 +10,9 @@ def from_file(target_file: str, template_file: str):
     :return: A spade TRoot cooresponding with a fully-formed template tree.
     """
     # Parse template file
-    ast = parser.TemplateParser.parse_file(template_file)
-    if not ast:
+    ast_root = parser.TemplateParser.parse_file(multistruct1)
+    if not ast_root:
         return None
 
-    # Find Ast root
-
-    # Generate template tree
-    return generator.generate_template(target_file, ast)
+    with open(target_file_path, "rb") as f:
+        return generator.generate_template(f, ast_root)
