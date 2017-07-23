dfc724de3c19a0cd8b050b8de575c20c10ec4f7f
Nyxxie <nyxxxxie@gmail.com>
2017-07-23 13:15:37 -0500

    Fixed from_file function better.

--- spade/template/__init__.py
+++ spade/template/__init__.py
@@ -2,15 +2,15 @@ import logging
 from spade.template import parser
 from spade.template import generator
 
-def from_file(target_file_path: str, template_file: str):
-    """Transforms a target + a .stf file into a workable template.
+def from_file(template_file_path: str, target_file_path: str):
+    """Transforms a .stf and target file into a workable template.
 
-    :param stf_file: STF file.
-    :param target_file: File to apply STF to.
+    :param template_file_path: Spade template file to apply to the target file.
+    :param target_file_path: File to apply the the template to.
     :return: A spade TRoot cooresponding with a fully-formed template tree.
     """
     # Parse template file
-    ast_root = parser.TemplateParser.parse_file(multistruct1)
+    ast_root = parser.TemplateParser.parse_file(template_file_path)
     if not ast_root:
         return None
 
